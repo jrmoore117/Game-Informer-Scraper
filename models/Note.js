91d1cde251ebb8aecba468085bfc6070db8e7362
jrmoore117 <jrmoore117@gmail.com>
2019-01-22 21:13:22 -0800

    Add post route for new notes and rough modal containing formm to enter new notes.

--- models/Note.js
+++ models/Note.js
@@ -3,10 +3,6 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var NoteSchema = new Schema({
-   title: {
-      type: String,
-      required: true
-   },
    body: {
       type: String,
       required: true
--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.set("view engine", "handlebars");
 // Root route to load index.handlebars with any artlces in database
 app.get("/", function (req, res) {
 
-   db.Article.find({saved: false})
-      .then(function(dbArticles) {
+   db.Article.find({ saved: false })
+      .then(function (dbArticles) {
 
          // console.log(dbArticles);
 
@@ -47,103 +47,104 @@ app.get("/", function (req, res) {
          // Render index.handlebars with Game Informer article data
          res.render("index", articles);
       })
-      .catch(function(err) {
+      .catch(function (err) {
          console.log("Error: ", err);
       })
 
 })
 
 // Route for scraping all front-page articles if database is empty
-app.get("/scrape", function(req, res) {
-   
-   db.Article.find({})
-   .then(function(dbContent) {
-      if(dbContent.length > 0) {
-         console.log("You have already scraped all the articles from Game Informer's home page!");
-      } else {
-         // Request Game Informer site. Scrape all article titles, summaries, authors, and urls.
-         request("https://www.gameinformer.com/", function (err, response, body) {
-            if (err) {
-               console.log("Error: ", error);
-            } else {
-      
-               var $ = cheerio.load(body);
-      
-               $(".article-summary").each(function (i, element) {
-      
-                  var articleInfo = {};
-      
-                  articleInfo.title = $(this)
-                     .children(".article-title")
-                     .children("a")
-                     .text()
-                     // Replaces all escape characters ("\n" and "\t") with an empty string
-                     .replace(/(\n)/g, "")
-                     .replace(/(\t)/g, "");
-      
-                  articleInfo.summary = $(this)
-                     .children(".promo-summary")
-                     .text();
-      
-                  articleInfo.author = $(this)
-                     .children(".author-details")
-                     .children("a")
-                     .text();
-      
-                  articleInfo.url = "https://www.gameinformer.com" + $(this)
-                     .children(".article-title")
-                     .children("a")
-                     .attr("href");
-                  
-                  db.Article.create(articleInfo)
-                     .then(function (dbArticle) {
-                        console.log(dbArticle);
-                     })
-                     .catch(function (err) {
-                        console.log(err);
-                     });
-               });
-            }
+app.get("/scrape", function (req, res) {
 
-            res.send("scrape complete!");
-         })
-      }
-   })
-   .catch(function(err) {
-      console.log(err);
-   })
+   db.Article.find({})
+      .then(function (dbContent) {
+         if (dbContent.length > 0) {
+            console.log("You have already scraped all the articles from Game Informer's home page!");
+         } else {
+            // Request Game Informer site. Scrape all article titles, summaries, authors, and urls.
+            request("https://www.gameinformer.com/", function (err, response, body) {
+               if (err) {
+                  console.log("Error: ", error);
+               } else {
+
+                  var $ = cheerio.load(body);
+
+                  $(".article-summary").each(function (i, element) {
+
+                     var articleInfo = {};
+
+                     articleInfo.title = $(this)
+                        .children(".article-title")
+                        .children("a")
+                        .text()
+                        // Replaces all escape characters ("\n" and "\t") with an empty string
+                        .replace(/(\n)/g, "")
+                        .replace(/(\t)/g, "");
+
+                     articleInfo.summary = $(this)
+                        .children(".promo-summary")
+                        .text();
+
+                     articleInfo.author = $(this)
+                        .children(".author-details")
+                        .children("a")
+                        .text();
+
+                     articleInfo.url = "https://www.gameinformer.com" + $(this)
+                        .children(".article-title")
+                        .children("a")
+                        .attr("href");
+
+                     db.Article.create(articleInfo)
+                        .then(function (dbArticle) {
+                           // console.log(dbArticle);
+                        })
+                        .catch(function (err) {
+                           console.log(err);
+                        });
+                  });
+               }
+
+               res.send("scrape complete!");
+            })
+         }
+      })
+      .catch(function (err) {
+         console.log(err);
+      })
 
 })
 
 // Route for deleting all articles from database
-app.get("/deleteArticles", function(req, res) {
+app.get("/deleteArticles", function (req, res) {
    db.Article.deleteMany({})
-      .then(function(result) {
+      .then(function (result) {
          console.log("Articles deleted!");
          res.json(result);
       })
-      .catch(function(err) {
+      .catch(function (err) {
          console.log(err);
       })
 })
 
 // Route for updating a specific article, marking it as saved when "Save Article" button is clicked
-app.put("/saveArticle/:id", function(req, res) {
+app.put("/saveArticle/:id", function (req, res) {
 
-   db.Article.findOneAndUpdate({_id: req.params.id}, {$set: {saved: true}})
-      .then(function(dbArticle) {
+   db.Article.findOneAndUpdate({ _id: req.params.id }, { $set: { saved: true } })
+      .then(function (dbArticle) {
          res.json(dbArticle);
       })
-      .catch(function(err) {
+      .catch(function (err) {
          console.log(err);
       })
 
 })
 
 // Route for rendering the "View Saved Articles" page
-app.get("/savedArticles", function(req, res) {
-   db.Article.find({saved: true})
-      .then(function(dbArticles) {
+app.get("/savedArticles", function (req, res) {
+
+   db.Article.find({ saved: true })
+      .then(function (dbArticles) {
 
          var articles = {
             articles: dbArticles
@@ -151,22 +152,41 @@ app.get("/savedArticles", function(req, res) {
 
          res.render("saved", articles);
       })
-      .catch(function(err) {
+      .catch(function (err) {
          console.log(err);
       })
+
 })
 
 // Route for 'unsaving' articles
-app.put("/unsaveArticle/:id", function(req, res) {
+app.put("/unsaveArticle/:id", function (req, res) {
 
-   db.Article.findOneAndUpdate({_id: req.params.id}, {$set: {saved: false}})
-      .then(function(dbArticle) {
+   db.Article.findOneAndUpdate({ _id: req.params.id }, { $set: { saved: false } })
+      .then(function (dbArticle) {
          res.json(dbArticle);
       })
-      .catch(function(err) {
+      .catch(function (err) {
          console.log(err);
       })
 
+})
+
+// Route for creating a new note
+app.post("/newNote/:id", function (req, res) {
+   console.log(req.params.id);
+   console.log(req.body);
+   
+   // Create a new note using the req.body
+   db.Note.create(req.body)
+      .then(function (dbNote) {
+         return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { notes: dbNote._id }}, { new: true });
+      })
+      .then(function (dbArticle) {
+         res.json(dbArticle);
+      })
+      .catch(function (err) {
+         res.json(err);
+      });
 })
 
 // =========================================================================================
